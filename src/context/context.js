9877275682ce73ef562473c40ec231c79ad81cbd
Piriyalini Niththiyanantham <priyalini.18@itfac.mrt.ac.lk>
2021-10-30 09:09:49 +0530

    eleventh commit

--- src/context/context.js
+++ src/context/context.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {linkData} from "./linkData";
 import { socialData } from './socialData';
+import {items} from './productData';
 const ProductContext = React.createContext();
 //Provider
 //Consumer
@@ -8,10 +9,66 @@ class ProductProvider extends Component{
 state={
     sidebarOpen:false,
     cartOpen:false,
-    cartItems:10,
     links:linkData,
     socialIcons:socialData,
-    cart:[]
+    cart:[],
+    cartItems:0,
+    cartSubTotal:0,
+    cartTax:0,
+    cartTotal:0,
+    storeProducts:[],
+    filteredProducts:[],
+    featuredProducts:[],
+    singleProduct:{},
+    loading:false
+};
+
+componentDidMount(){
+    //from contentful items
+
+    this.setProducts(items);
+}
+
+//set products
+setProducts = (products) =>{
+    let storeProducts = products.map(item =>{
+        const {id} = item.sys;
+        const image = item.fields.image.fields.file.url;
+        const product = {id,...item.fields,image};
+        return product;
+    });
+    //featured products
+    let featuredProducts=storeProducts.filter(item => item.featured===true);
+    this.setState({
+        storeProducts,
+        filteredProducts:storeProducts,
+        featuredProducts,
+        cart:this.getStorageCart(),
+        singleProduct:this.getStorageProduct(),
+        loading:false
+    })
+};
+// get cart from local storage
+getStorageProduct=()=>{
+    return [];
+};
+// get product from local Storage
+getStorageCart=()=>{
+    return {};
+};
+// get totals
+getTotals=()=>{};
+// add totals
+addTotals=()=>{};
+// sync storage
+syncStorage=()=>{};
+// add to cart
+addToCart=(id)=>{
+    console.log(`add to cart ${id}`);
+};
+// set single product
+setSingleProduct = (id)=>{
+    console.log(`set single product ${id}`);
 }
 //handle sidebar
 handleSidebar=()=>{
@@ -38,7 +95,9 @@ openCart=()=>{
             handleSidebar:this.handleSidebar,
             handleCart:this.handleCart,
             closeCart:this.closeCart,
-            openCart:this.openCart
+            openCart:this.openCart,
+            addToCart:this.addToCart,
+            setSingleProduct:this.setSingleProduct
             }}>
             {this.props.children}
         </ProductContext.Provider>

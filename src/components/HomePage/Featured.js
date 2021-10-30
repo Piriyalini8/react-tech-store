cabc024f62220694e997e1b63529a47f27df31ca
Piriyalini Niththiyanantham <priyalini.18@itfac.mrt.ac.lk>
2021-10-30 09:26:55 +0530

    twelveth commit

--- src/components/HomePage/Featured.js
+++ src/components/HomePage/Featured.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Product from '../Product';
+import { Link } from 'react-router-dom';
+import Title from '../Title';
+import {ProductConsumer} from '../../context';
 
 export default function Featured() {
     return (
-        <div>
-            Hello From Featured
-        </div>
+        <section className="py-5">
+            <div className="container">
+                {/* title */}
+                <Title title="featured products" center="true"/>
+                {/* products */}
+                <div className="row">
+                    <ProductConsumer>
+                        {value =>{
+                            const {featuredProducts} = value;
+                            return featuredProducts.map(product =>(
+                                <Product key={product.id} product={product}/>
+                            ));
+                        }}
+                    </ProductConsumer>
+                </div>
+            </div>
+        </section>
     )
 }

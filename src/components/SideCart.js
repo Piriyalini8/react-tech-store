96042cf69a7bac6cc1b486bbb83437f4a2d6e4ac
Piriyalini Niththiyanantham <priyalini.18@itfac.mrt.ac.lk>
2021-11-01 10:00:37 +0530

    ninteenth commit

--- src/components/SideCart.js
+++ src/components/SideCart.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import { ProductConsumer } from '../context/context';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export default function SideCart() {
     return (
         <ProductConsumer>
             {value => {
-                const {cartOpen, closeCart, cart} = value;
+                const {cartOpen, closeCart, cart, cartTotal} = value;
                 return <CartWrapper show={cartOpen} 
                 onClick={closeCart}>
-                    <p>cart items</p>
+                    <ul>
+                        {cart.map(item =>{
+                            return (
+                                <li key={item.id} className="cart-item mb-4">
+                                    <img width="35" 
+                                    src={`../${item.image}`}
+                                    // src={item.image}   will see in contentful
+                                    alt="cart-item"/>
+                                    <div className="mt-3">
+                                        <h6 className="text-uppercase">{item.title}</h6>
+                                        <h6 className="text-title text-capitalize">
+                                            amount : {item.count}
+                                        </h6>
+                                    </div>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                    <h4 className="text-capitalize text-main">
+                        cart total : $ {cartTotal}
+                    </h4>
+                    <div className="text-center my-5">
+                        <Link to="/cart" className="main-link">
+                            cart page
+                        </Link>
+                    </div>  
                 </CartWrapper>
             }}
         </ProductConsumer>
@@ -29,6 +55,14 @@ transition: var(--mainTransition);
 transform:${props => props.show? "translateX(0)":"translateX(100%)" } ;
 @media (min-width : 576px){
     width: 20rem;
+}
+overflow: scroll;
+padding: 2rem;
+ul{
+    padding: 0 !important;
+}
+.cart-item{
+    list-style-type: none;
 }
 
 `
